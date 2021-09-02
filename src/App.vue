a1f7daa1fe8fb154f1a3d826ede6eaddc8001a77
Ricardo Cuellar <rcuellarsn@gmail.com>
2021-09-02 18:17:27 -0500

    Add basic components for the app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img class="fade-in" alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <PokemonPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import PokemonPage from './pages/PokemonPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokemonPage
+    //HelloWorld
   }
 }
 </script>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>¿Quién es este pokémon?</h1>
+    <!--TODO img-->
+    <PokemonPicture />
+
+    <PokemonOptions />
+    <!--TODO opciones-->
+    <!-- En VUE 2 se pone en un div<div></div>-->
+</template>
+
+<script>
+
+import PokemonPicture from '@/components/PokemonPicture'
+import PokemonOptions from '@/components/PokemonOptions'
+
+
+export default {
+    name: 'PokemonPage',
+    components: {
+        PokemonPicture,
+        PokemonOptions
+    }
+    
+}
+</script>
+
