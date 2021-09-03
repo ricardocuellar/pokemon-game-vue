85d86aae77e6a38532ed6e869bfc2264419bec09
Ricardo Cuellar <rcuellarsn@gmail.com>
2021-09-02 19:23:10 -0500

    Add logic for get pokemons names and return four options

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,4 +1,5 @@
 
+import pokemonApi from '@/api/pokemonApi'
 
 const getPokemons = () =>{
 
@@ -10,16 +11,39 @@ const getPokemons = () =>{
 
 }
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async() => {
     
     const mixedPokemons = getPokemons().sort(() => Math.random() - 0.5)
     
-    getPokemonsNames( mixedPokemons.splice(0,4) )
+   const pokemons = await getPokemonsNames( mixedPokemons.splice(0,4) )
+   //console.table(pokemons)
+
+   return pokemons
+
 }
 
 
-const getPokemonsNames = ( [a,b,c,d] = []) =>{
-    console.log(a,b,c,d)
+const getPokemonsNames = async( [a,b,c,d] = []) =>{
+    // const resp = await pokemonApi.get(`/1`)
+    // console.log(resp)
+    //console.log(a,b,c,d)
+
+    const promiseArr = [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`),
+    ]
+
+    //Promesas simultaneas
+    const [p1,p2,p3,p4] = await Promise.all( promiseArr )
+    
+    return [
+        {name: p1.data.name, id: p1.data.id},
+        {name: p2.data.name, id: p2.data.id},
+        {name: p3.data.name, id: p3.data.id},
+        {name: p4.data.name, id: p4.data.id},
+    ]
 }
 
 
